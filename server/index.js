e36cbbaf4672acb031f21ae1f6dce848527a81d5
manish257 <manishmahato257@gmail.com>
2022-07-14 12:19:55 +0500

    populating data in db

--- server/index.js
+++ server/index.js
@@ -1,18 +1,31 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-//const pool = require('./db');
+const pool = require('./db');
 
 
 
 //middleware
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //req.body
 
 // Routes
 
 // create a todo
+
+app.post("/todos", async (req,res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query(
+            "INSERT INTO todo (description) VALUES($1)",
+            [description]
+        );
+        res.json(newTodo);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
 
 // get all todos
 
